486b69622ae81159911027d0c573d8ad0769cdcf
Ibex02 <jojo.methi@gmail.com>
2021-01-22 19:46:49 +0100

    test

--- src/Berechnung.java
+++ src/Berechnung.java
@@ -2,10 +2,10 @@ public class Berechnung {
     static double m = 0.4;//mass
     static double t = 100; //thrust
     static double r = 0.3; //distance engine center of mass
-    static double l = 0.5; //lenght of the rocket
+    static double l = 0.5; //length of the rocket
     static double dax = 25; //deflection Angle at the beginning on X-Axis
     static double day = 5; //deflection Angle at the beginning on Y-Axis
-    static double dt =0.05; //delta t = time between tow meassurements
+    static double dt =0.05; //delta t = time between tow measurements
     static double ct = 0; //time since the simulation begun in s
     static double at = 5;//time the simulation will run in seconds
     static double k;
@@ -25,7 +25,7 @@ public class Berechnung {
         while(ct<at){
             double eax = cax;
             sax = P*eax + D* ((eax-pax)/dt);
-            System.out.println("Servo"+sax);
+            System.out.println("Servo "+sax);
             cax = k*t* Math.sin(sax)*(dt*dt)+cax;
             cay = k*t* Math.sin(say)*(dt*dt)+cay;
             System.out.println(cax);
