7bbddf45eea213c64628828ae0ed0c3b700d895b
IntelBeat <frederickgruhn@web.de>
2021-01-23 12:46:33 +0100

    jo

--- src/Berechnung.java
+++ src/Berechnung.java
@@ -1,17 +1,17 @@
 public class Berechnung {
     static double m = 0.4;//mass
-    static double t = 100; //thrust
+    static double t = 10; //thrust
     static double r = 0.3; //distance engine center of mass
-    static double l = 0.5; //length of the rocket
-    static double dax = 25; //deflection Angle at the beginning on X-Axis
+    static double l = 0.5; //lenght of the rocket
+    static double dax = 55; //deflection Angle at the beginning on X-Axis
     static double day = 5; //deflection Angle at the beginning on Y-Axis
-    static double dt =0.05; //delta t = time between tow measurements
+    static double dt =0.05; //delta t = time between tow meassurements
     static double ct = 0; //time since the simulation begun in s
     static double at = 5;//time the simulation will run in seconds
     static double k;
-    static double P = 8;
+    static double P = 1;
     static double I = 0.01;
-    static double D = 100;
+    static double D = 1;
 
     public static void main(String[] args){
         double sax = 0; //current angele of servos
@@ -20,17 +20,17 @@ public class Berechnung {
         double cay; //current angle on Y-Axis
         double pax = 0; // previous X_Axis angle
         k =  (24*r)/(m*(l*l));
-        cax = dax;
+        cax = (dax*Math.PI)/180;
         cay = day;
         while(ct<at){
             double eax = cax;
-            sax = P*eax + D* ((eax-pax)/dt);
-            System.out.println("Servo "+sax);
+            sax = P*eax + D *((((eax-pax)*Math.PI)/180)/dt);
             cax = k*t* Math.sin(sax)*(dt*dt)+cax;
-            cay = k*t* Math.sin(say)*(dt*dt)+cay;
-            System.out.println(cax);
+            cay = k*t *Math.sin(say)*(dt*dt)+cay;
+            System.out.println((cax*180)/Math.PI);
             ct = ct + dt;
             pax = cax;
         }
     }
 }
+
